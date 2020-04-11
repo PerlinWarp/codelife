5e4ca5e3de85d124d1b498973a2bd32badb2fe9d
perlinwarp@gmail.com <perlinwarp@gmail.com>
2020-04-11 16:53:07 +0100

    Simple agent modified

--- agent.py
+++ agent.py
@@ -16,25 +16,15 @@ class Agent:
         self.life = max_life
         self.alive_time = 0
 
-    def see(self,grid):
-        c = grid.get_cell(self.x,self.y)
-
     def eat(self, grid):
         c = grid.get_cell(self.x,self.y) 
         if (c.life > 10):
             c.life -= 10
             self.life += 10
 
-
-    def move(self):
-        if(random.random() < 0.01):
-            self.x += agent_size
-        if(random.random() < 0.02):
-            self.x -= agent_size
-        if(random.random() < 0.03):
-            self.y += agent_size
-        if(random.random() < 0.04):
-            self.y -= agent_size
+    def move(self, delta_x, delta_y):
+        self.x += delta_x
+        self.y += delta_y
 
         # Sanity checks
         if(self.x < 0):
@@ -46,13 +36,30 @@ class Agent:
         if (self.y + agent_size> w_height):
             self.y = w_height
 
+    def live(self, grid):
+        # The brains of the agent
+        c = grid.get_cell(self.x,self.y)
+
+        # Calculate delta_x and delta_y
+        delta_x = 0
+        delta_y = 0
+        if(random.random() < 0.01):
+            delta_x = agent_size
+        if(random.random() < 0.02):
+            delta_x = -agent_size
+        if(random.random() < 0.03):
+            delta_y = agent_size
+        if(random.random() < 0.04):
+            delta_y = -agent_size
+        self.move(delta_x,delta_y)
+
     def run(self, grid):
         self.life -= 1
         self.alive_time += 1
         if (self.life > max_life):
             self.life = max_life
-            
-        self.move()
+
+        self.live(grid)
         self.eat(grid)
 
     def draw(self,screen):
